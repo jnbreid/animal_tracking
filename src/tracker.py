f59e02280102c5aabfa3fe4bd7daa1409e7a913d
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-03-16 20:09:34 +0100

    tracker finished

--- src/tracker.py
+++ src/tracker.py
@@ -3,10 +3,12 @@ import numpy as np
 import cv2
 from pycocotools import mask
 import time
+import os
 
 from utils_loader import get_detector, get_segmentor, get_extractor, get_DistNet
 from utils import overlay, col_list, merge_masks_fast
 from track import Tracker
+from utils_data import write_mp4
 
 
 def infer_video(img_seq, 
@@ -22,9 +24,16 @@ def infer_video(img_seq,
                 segmentor = None, 
                 device = None, 
                 refine = True, 
-                dist_mode = 'default'):
+                dist_mode = 'default',
+                fps = 12):
     
     start_time = time.time()
+
+    print(f"Video processing started.")
+    os.mkdir(save_dir)
+    if visualize == True:
+        image_dir = os.path.join(save_dir, 'images')
+        os.mkdir(image_dir)
 
     if device is None:
         device = 'cpu'
@@ -42,18 +51,15 @@ def infer_video(img_seq,
     mot_tracker = Tracker(distnet, max_age = 15, min_hits = 3, iou_threshold = 0.1, dist_mode = dist_mode)
     colors = col_list()
 
-
-    if box_file == True:
-        full_tracks = []
-    if seg_file == True:
-        mots_string = ""
+    full_tracks = []
+    mots_string = ""
 
     for i in range(len(img_seq)):
-        print(f"Frame {i+1} / {len(img_seq)}")
+        if i % 10 == 0:
+            print(f"Frame {i+1} / {len(img_seq)}")
         
         current_image = img_seq[i]
-        if seg_file == True:
-            full_mask = np.zeros((current_image.shape[:2]))
+        full_mask = np.zeros((current_image.shape[:2]))
 
         # detection
         if bbox_p is not None:
@@ -100,6 +106,7 @@ def infer_video(img_seq,
         save_detections = []
 
         for j in range(tracks.shape[0]):
+            item = tracks[j,:]
             x0 = int(item[0])
             y0 = int(item[1])
             x1 = int(item[2])
@@ -128,111 +135,78 @@ def infer_video(img_seq,
             full_tracks.append([i, item[4], box[0], box[1], box[2]-box[0], box[3]-box[1], c_score, -1, -1, -1])
 
             # calculate mask image for mota file
-            if seg_file == True:
-                full_mask = merge_masks_fast(full_mask, pred_mask, item[4])
+            full_mask = merge_masks_fast(full_mask, pred_mask, item[4])
         
         if visualize == True:
             bgr_img = cv2.cvtColor(current_image.copy(), cv2.COLOR_RGB2BGR)
 
-        if seg_file == True:
-            c_id_list = list(np.unique(full_mask))
-            c_id_list.remove(0)
+        
+        c_id_list = list(np.unique(full_mask))
+        c_id_list.remove(0)
 
-            for k in range(len(c_id_list)):
-                wanted_id = c_id_list[k]
-                c_mask = np.zeros(full_mask.shape, dtype = np.uint8)
-                c_mask[full_mask==wanted_id] = 1
+        for k in range(len(c_id_list)):
+            wanted_id = c_id_list[k]
+            c_mask = np.zeros(full_mask.shape, dtype = np.uint8)
+            c_mask[full_mask==wanted_id] = 1
 
-                vis_mask = c_mask.copy()
+            vis_mask = c_mask.copy()
 
-                c_mask = np.asfortranarray(c_mask)
+            c_mask = np.asfortranarray(c_mask)
 
-                rle = mask.encode(c_mask)
-                size = rle['size']
-                counts = rle['counts'].decode('UTF-8')
-                mots_string = mots_string + f"{str(i)} {str(1000 + int(wanted_id))} {str(1)} {str(size[0])} {str(size[1])} {counts}\n"
+            rle = mask.encode(c_mask)
+            size = rle['size']
+            counts = rle['counts'].decode('UTF-8')
+            mots_string = mots_string + f"{str(i)} {str(1000 + int(wanted_id))} {str(1)} {str(size[0])} {str(size[1])} {counts}\n"
 
-                if visualize == True:
-                    col = colors.get_color(k)
-                    bgr_img = overlay(bgr_img, vis_mask, (int(col[2]),int(col[1]),int(col[0])), alpha = 0.5)
-                    
-                    
-                    refined_indexes = np.where(vis_mask == 1)
-                    ref_x0 = int(refined_indexes[0].min())
-                    ref_x1 = int(refined_indexes[0].max())
-                    ref_y0 = int(refined_indexes[1].min())
-                    ref_y1 = int(refined_indexes[1].max())
-                    box = np.asarray([ref_y0, ref_x0, ref_y1,  ref_x1])
-                    cv2.rectangle(bgr_img, (box[0], box[1]), (box[2], box[3]), (int(col[2]),int(col[1]),int(col[0])), 4)
-                    cv2.putText(bgr_img, '#'+track_label, (x0+5, y0-10), 0,0.6,(int(col[2]),int(col[1]),int(col[0])),thickness=4)
+            if visualize == True:
+                col = colors.get_color(k)
+                bgr_img = overlay(bgr_img, vis_mask, (int(col[2]),int(col[1]),int(col[0])), alpha = 0.5)
+                
 
-#################################################################
-img_seq, confidences, bbox, c_id = dset.__getitem__(elem)
+                refined_indexes = np.where(vis_mask == 1)
+                ref_x0 = int(refined_indexes[0].min())
+                ref_x1 = int(refined_indexes[0].max())
+                ref_y0 = int(refined_indexes[1].min())
+                ref_y1 = int(refined_indexes[1].max())
+                box = np.asarray([ref_y0, ref_x0, ref_y1,  ref_x1])
+                cv2.rectangle(bgr_img, (box[0], box[1]), (box[2], box[3]), (int(col[2]),int(col[1]),int(col[0])), 4)
+                cv2.putText(bgr_img, '#'+track_label, (x0+5, y0-10), 0,0.6,(int(col[2]),int(col[1]),int(col[0])),thickness=4)
 
+        # save generated annotated frames    
+        if visualize == True:
+            img_name = f"str(i).zfill(6).jpg"
+            img_path = os.path.join(image_dir, img_name)
+            cv2.imwrite(img_path, bgr_img)
 
-for i in range(len(confidences)):
-full_detect = []
-c_masks = []
-c_feature_vects = []
-
+                
 
 
-if current_boxes.shape[0] == 0:
-    full_detect = np.empty((0, 5))
-    c_masks = np.asarray([])
-    c_feature_vects = np.asarray([])
 
-tracks = mot_tracker.update(np.array(full_detect), c_masks, c_feature_vects)
-#print(np.array(tracks[:, 0:4], dtype = np.int8))
 
-#print(np.array(tracks, dtype = np.int16))
-masks = []
-save_detections = []
+    #save mot16 style annotation file
+    if box_file == True:
+        print(f"Saving segmentation masks in mots txt file.")
+        pred_array = np.asarray(full_tracks)
+        pred_path = os.path.join(save_dir, f"mot16.txt") 
+        np.savetxt(pred_path, pred_array, delimiter=',')
 
-for j in range(tracks.shape[0]):
-    item = tracks[j,:]
-    x0 = int(item[0])
-    y0 = int(item[1])
-    x1 = int(item[2])
-    y1 = int(item[3])
-    track_label = str(int(item[4]))
+    #save mots annotation file
+    if seg_file == True:
+        print(f"Saving segmentation masks in mots txt file.")
+        mots_save_path = os.path.join(save_dir, f"mots.txt")
+        with open(mots_save_path, "w") as text_file:
+            text_file.write(mots_string)
 
-    #  --------------frame, id, b_left, b_top, b_width, b_height, conf, x, y, z
-    # with x = y = z = -1 as there are no 3d coordinates (here conf is always 1 beceause we use original annotations)
+    #save annotated video file
+    if visualize == True:
+        print(f"Generating and saving mp4 video file ...")
+        video_path = os.path.join(save_dir, 'annotated.mp4')
+        image_list = os.listdir(image_dir)
+        image_list.sort()
+        image_list = [os.path.join(image_dir, item) for item in image_list]
+        write_mp4(video_path, image_list, fps = fps) 
+        print(f"Annotated video saved.")
 
-    box = np.asarray([x0,y0,x1,y1])
-    pred_mask, _, _ = mask_predictor.predict(box=box, multimask_output=False)
-    pred_mask = pred_mask[0,:,:].astype(np.int8)
-    # refine box
-    if refine == True:
-    refined_indexes = np.where(pred_mask == 1)
 
-    ref_x0 = int(refined_indexes[0].min())
-    ref_x1 = int(refined_indexes[0].max())
-
-    ref_y0 = int(refined_indexes[1].min())
-    ref_y1 = int(refined_indexes[1].max())
-
-    box = np.asarray([ref_y0, ref_x0, ref_y1,  ref_x1])
-
-    masks.append(pred_mask)
-    save_detections.append([item[4], box[0], box[1], box[2]-box[0], box[3]-box[1]])
-
-    c_score = scores[j]
-
-    full_tracks.append([i, item[4], box[0], box[1], box[2]-box[0], box[3]-box[1], c_score, -1, -1, -1])
-
-    #curr_img = overlay(img_bgr, pred_mask, (0,255,0), alpha = 0.5)
-    #cv2.rectangle(curr_img, (x0, y0), (x1, y1), (0,255,255), 4)
-    #cv2.putText(curr_img, '#'+track_label, (x0+5, y0-10), 0,0.6,(0,255,255),thickness=4)
-masks = np.asarray(masks)
-#np.save(mask_file_path, masks)
-save_detecs = np.asarray(save_detections)
-#np.save(detec_file_path, save_detecs)
-#cv2.imwrite(new_img_path, curr_img)
-
-pred_array = np.asarray(full_tracks)
-
-pred_path = save_path + str(c_id) + '.txt'#str(elem) + '.txt'
-print(pred_path)#, img_seq[0])
-np.savetxt(pred_path, pred_array, delimiter=',')
+    end_time = time.time()
+    print(f"Task finished. Total time needed (in seconds): {end_time-start_time}.")
